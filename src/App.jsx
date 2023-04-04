6f384ef8f66669ea3ee9921e16ecdf7c9f54a8be
asifwebdeveloper <128634807+asifwebdeveloper@users.noreply.github.com>
2023-04-04 21:04:20 +0300

    New  Calc

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Router,Routes,BrowserRouter } from 'react-router-dom';
+import Home from "./pages/home"
+function App() {
+  return (
+    <>
+     <div className="App">
+      
+    </div>
+    </>
+   
+  );
+}
+
+export default App;
